02e880f7b1a8c061b06944653044a6bb089247d1
KingJale <WKingJale@gmail.com>
2026-03-01 13:32:49 +0800

    学会了nn.Sequential方法，以及子类父类的一些简单知识

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -5,15 +5,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class DoubleConv(nn.Module):
+class DoubleConv(nn.Module):#继承父类
     """(convolution => [BN] => ReLU) * 2"""
 
     def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
+        super().__init__()#同步父类的属性
         if not mid_channels:
             mid_channels = out_channels
-        self.double_conv = nn.Sequential(
+        self.double_conv = nn.Sequential(#子类直接使用父类中的Sequential方法来构建网络
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            #in_channels(int):输入特征图的通道数（上一层输出的通道数）
+            #mid_channels(int)输出特征图的通道数(即卷积核的数量)，决定了这一层提取多少不同的特征
+            #kernel_size(int or tuple(元组)):卷积核大小，可以写成（3，3）或单个数字3
+            #padding(int or tuple):在输入特征图周围填充的像素层数，padding=1表示上下左右各填充1行/列
+            #bias(bool):是否使用偏置项(就是y=ax+b中的b)，默认为True。这里不使用偏置项的原因1后面会接BatchNorm层(本身有可学习的放缩和平移参数)
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
@@ -23,6 +28,16 @@ class DoubleConv(nn.Module):
 
     def forward(self, x):
         return self.double_conv(x)
+    """nn.Sequential可以允许将多个模块封装成一个模块，
+    forward函数接收输入之后，nn.Sequential按照内部模块的顺序自动依次计算并输出结果。
+    nn.Sequential内部实现了forward函数，因此可以不用写forward函数。"""
+
+
+
+
+
+
+
 
 
 class Down(nn.Module):
