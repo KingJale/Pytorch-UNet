4c854ef7f7a0f5c717e2d96ff99f8176827a7213
KingJale <WKingJale@gmail.com>
2026-03-01 13:44:47 +0800

    学习了这个按序编码的模型的代码，了解了参数含义，网络使用设置

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 
 class DoubleConv(nn.Module):#继承父类
-    """(convolution => [BN] => ReLU) * 2"""
+    """(convolution => [BN] => ReLU) * 2"""#卷积->批归一化->激活函数，进行了两次
 
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()#同步父类的属性
@@ -20,7 +20,23 @@ class DoubleConv(nn.Module):#继承父类
             #padding(int or tuple):在输入特征图周围填充的像素层数，padding=1表示上下左右各填充1行/列
             #bias(bool):是否使用偏置项(就是y=ax+b中的b)，默认为True。这里不使用偏置项的原因1后面会接BatchNorm层(本身有可学习的放缩和平移参数)
             nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
+            #批归一化是一种深度神经网络训练技巧，通过对每一批（batch）数据进行规范化处理，加速网络训练、提高模型稳定性。
+                                                    """
+                                                    为什么要用批归一化？
+                                                            问题：内部协变量偏移（Internal Covariate Shift）
+                                                            网络每一层的输入分布会随着训练而不断变化
+                                                            导致网络需要不断调整来适应新的数据分布
+                                                            训练过程变得不稳定，收敛慢
+                                                            解决方案：批归一化的作用
+                                                            稳定训练：保持各层输入分布稳定
+                                                            加速收敛：允许使用更大的学习率
+                                                            防止梯度消失/爆炸：控制激活值的范围
+                                                            轻微的正则化效果：引入小批量统计噪声
+                                                    """
+            nn.ReLU(inplace=True),#inplace=True表示原地操作，即 ReLU 函数直接修改输入张量本身，而不是创建一个新的张量。
+            #以前的激活函数（如sigmoid、tanh）在输入值较大时梯度接近0
+            #ReLU因其简单高效成为深度学习中最主流的激活函数。虽然存在"神经元死亡"等问题，但配合批归一化和合理的初始化，在大多数情况下表现优异。它是现代深度神经网络能训练得又快又好的关键技术之一。
+            #Leaky ReLU​ - 解决"神经元死亡"问题
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
